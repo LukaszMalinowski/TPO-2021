657a0a51ebbca270a51dfeaae8d69f47aad60625
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-03-14 23:23:01 +0100

    complete exercise 1 TPO1

--- TPO1_ML_S19743/src/zad1/Futil.java
+++ TPO1_ML_S19743/src/zad1/Futil.java
@@ -1,8 +1,65 @@
 package zad1;
 
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class Futil {
 
     public static void processDir(String dirName, String resultFileName) {
+        try {
+            clearResultFile(resultFileName);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
+        try {
+            FileChannel outChannel = new FileOutputStream(resultFileName).getChannel();
+            final long[] previousPosition = {0};
+
+            Files.walkFileTree(Paths.get(dirName), new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.getFileName().toString().equals(resultFileName))
+                        return FileVisitResult.CONTINUE;
+
+                    if (file.toString().endsWith(".txt")) {
+                        FileChannel inChannel = new FileInputStream(file.toFile()).getChannel();
+                        outChannel.transferFrom(inChannel, previousPosition[0], inChannel.size());
+                        previousPosition[0] = outChannel.size();
+                        inChannel.close();
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            outChannel.close();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void clearResultFile(String resultFileName) throws IOException {
+        Files.deleteIfExists(Paths.get(resultFileName));
+        Files.createFile(Paths.get(resultFileName));
     }
 }

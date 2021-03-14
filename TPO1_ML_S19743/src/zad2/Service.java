a714d0a94b6d8525b3960de1a9e1d18ed6f27787
Lukasz Malinowski <s19743@pjwstk.edu.pl>
2021-03-14 21:32:08 +0100

    add tpo-1

---
+++ TPO1_ML_S19743/src/zad2/Service.java
@@ -0,0 +1,28 @@
+/**
+ *
+ *  @author Malinowski Łukasz S19743
+ *
+ */
+
+package zad2;
+
+
+public class Service {
+
+    private String country;
+
+    public Service(String country) {
+        this.country = country;
+    }
+
+    public String getWeather(String city) {
+        return null;
+    }
+
+    public Double getRateFor(String currencyCode) {
+        return null;
+    }
+    public Double getNBPRate() {
+        return null;
+    }
+}  

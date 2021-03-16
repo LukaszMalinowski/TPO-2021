23caecfa5be099f0612772e90506a5dc6fcdd551
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-03-16 10:25:58 +0100

    implement getWeather() function

--- TPO1_ML_S19743/src/zad2/Service.java
+++ TPO1_ML_S19743/src/zad2/Service.java
@@ -1,27 +1,47 @@
 /**
- *
- *  @author Malinowski Łukasz S19743
- *
+ * @author Malinowski Łukasz S19743
  */
 
 package zad2;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
+import java.io.IOException;
 
 public class Service {
+    private final String WEATHER_API_KEY = "44f6dad7325facf5be789bcb3678ab09";
+
+    private final String WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s";
+    private final String EXCHANGE_API_URL = "https://api.exchangeratesapi.io/latest?base=%s";
 
     private String country;
+    OkHttpClient client;
 
     public Service(String country) {
         this.country = country;
+        this.client = new OkHttpClient();
     }
 
     public String getWeather(String city) {
-        return null;
+        String url = String.format(WEATHER_API_URL, city, WEATHER_API_KEY);
+        Request request = new Request.Builder().url(url).method("GET", null).build();
+        String response = "";
+        try {
+            response = client.newCall(request).execute().body().string();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
     }
 
     public Double getRateFor(String currencyCode) {
+        String url = String.format(EXCHANGE_API_URL, currencyCode);
         return null;
     }
+
     public Double getNBPRate() {
         return null;
     }

02729766a22825ac2cdfa0d82701292b5086a3d1
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-04-18 19:32:25 +0200

    Added ServerSocketChannel

--- TPO2_ML_S19743/src/zad1/ChatClientTask.java
+++ TPO2_ML_S19743/src/zad1/ChatClientTask.java
@@ -43,7 +43,7 @@ public class ChatClientTask implements Runnable {
     }
 
     public ChatClient getClient() {
-        return null;
+        return client;
     }
 
     private void sleep(int wait) {
--- TPO2_ML_S19743/src/zad1/ChatServer.java
+++ TPO2_ML_S19743/src/zad1/ChatServer.java
@@ -7,10 +7,24 @@
 package zad1;
 
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 public class ChatServer {
 
     private String host;
     private int port;
+    boolean serverIsRunning;
+    private ServerSocketChannel serverChannel;
+    private Selector selector;
+    private final List<String> serverLog = Collections.synchronizedList(new ArrayList<>());
 
     public ChatServer(String host, int port) {
         this.host = host;
@@ -19,10 +33,45 @@ public class ChatServer {
 
     public void startServer() {
         System.out.println("Server started");
+        try {
+            serverChannel = ServerSocketChannel.open();
+            serverChannel.socket().bind(new InetSocketAddress(host, port));
+            serverChannel.configureBlocking(false);
+            selector = Selector.open();
+
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        serviceConnections();
+    }
+
+    private void serviceConnections() {
+        serverIsRunning = true;
+
+        while (serverIsRunning) {
+            try {
+                selector.select();
+
+                Set<SelectionKey> keys = selector.selectedKeys();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
     public void stopServer() {
         System.out.println("Server stopped");
+        try {
+            serverChannel.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
